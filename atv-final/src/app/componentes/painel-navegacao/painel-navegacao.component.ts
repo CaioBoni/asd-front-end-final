e010dd93e8eeb94d7d06c23c168aecd5ee10ea67
Caio <caio.goulart.n@gmail.com>
2019-11-25 21:32:42 -0300

    api

--- atv-final/src/app/componentes/painel-navegacao/painel-navegacao.component.ts
+++ atv-final/src/app/componentes/painel-navegacao/painel-navegacao.component.ts
@@ -9,33 +9,25 @@ import { FilmeService } from 'src/app/service/filme.service';
 export class PainelNavegacaoComponent implements OnInit {
   filtro;
   cards;
-
-  datasource = [
-    { title: 'Card 1', cols: 1, rows: 1 },
-    { title: 'Card 11', cols: 1, rows: 1 },
-    { title: 'Card 2', cols: 1, rows: 1 },
-    { title: 'Card 4', cols: 1, rows: 1 }
-  ];
+  datasource = [];
 
   constructor(private filmeService: FilmeService) { }
 
   ngOnInit(): void {
-    this.cards = this.datasource;
     this.filmeService.listarFilmes().subscribe(retorno => {
       if (retorno) {
         console.log(retorno);
+        this.datasource = retorno.results;
+        this.cards = this.datasource;
       } else {
         window.alert("Não foi possível consultar lista de filmes");
       }
     });
   }
 
-  //TODO - INSERIR FILTROS CORRETOS
   filtrar() {
-    this.cards = this.cards.filter(card => card.title.indexOf(this.filtro) >= 0);
-    if (this.filtro == undefined || this.filtro.length == 0) {
-      this.cards = this.datasource;
-    }
+    this.cards = this.datasource;
+    this.cards = this.cards.filter(card => card.title.toUpperCase().indexOf(this.filtro.toUpperCase()) >= 0);
   }
 
 }
--- atv-final/src/app/service/filme.service.ts
+++ atv-final/src/app/service/filme.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const FILMES_POPULARES = "/movies/popular"
-const PESQUISAR_FILME = "/search/movie"
-const BASE_URL = environment.url;
+const PESQUISAR_FILME = "https://api.themoviedb.org/3/movie/330457?api_key=0b2bde81e23eda2d0229800eb23237e4";
+const FILMES_POPULARES = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0b2bde81e23eda2d0229800eb23237e4";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmeService {
-  listaFilme: [];
+  listaFilme:any[] = [];
 
   constructor(private http: HttpClient) { }
 
-  listarFilmes() {
-    return this.get(`${BASE_URL}${FILMES_POPULARES}`);
+  listarFilmes() { //${BASE_URL}
+    return this.get(`${FILMES_POPULARES}`, "");
   }
 
   buscarFilme(idFilme) {
-    return this.post(`${BASE_URL}${PESQUISAR_FILME}`, { id: idFilme});
+    return this.post(`${PESQUISAR_FILME}`, { id: idFilme});
   }
 
-  get(
-    path: string,
-    httpParams?: HttpParams | {
-      [param: string]: string | string[];
-    }): Observable<any> {
-    return this.http.get(BASE_URL + path, { params: httpParams, reportProgress: true });
+  get(url, param): Observable<any> {
+    return this.http.get(url, {
+      params: {
+        sort_by: "popularity.desc",
+        api_key: "0b2bde81e23eda2d0229800eb23237e4"
+      }
+    });
+
+      //BASE_URL + path, { params: httpParams, reportProgress: true });
   }
 
   post(
@@ -37,7 +39,7 @@ export class FilmeService {
     httpParams?: HttpParams | {
       [param: string]: string | string[];
     }): Observable<any> {
-    return this.http.post(BASE_URL + path, body, { params: httpParams, reportProgress: true });
+    return this.http.post(path, body, { params: httpParams, reportProgress: true });
   }
 
 }
